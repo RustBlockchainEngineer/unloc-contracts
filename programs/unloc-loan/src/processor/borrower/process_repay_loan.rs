7ab0c6a4f831009aee9002be6472d82547e3f09c
jwtradera <jwtradera@outlook.com>
2022-06-15 02:13:52 -0700

    Update min_repaid_numerator with global state

--- programs/unloc-loan/src/processor/borrower/process_repay_loan.rs
+++ programs/unloc-loan/src/processor/borrower/process_repay_loan.rs
@@ -26,12 +26,17 @@ pub fn handle(ctx: Context<RepayLoan>) -> Result<()> {
     let current_time = ctx.accounts.clock.unix_timestamp as u64;
     let seconds_for_year = 3600 * 24 * 365;
     let offer_apr = ctx.accounts.sub_offer.apr_numerator;
+    let min_repaid_numerator = ctx.accounts.global_state.min_repaid_numerator;
     let unloc_apr = ctx.accounts.global_state.apr_numerator;
     let denominator = ctx.accounts.global_state.denominator;
     let accrued_apr = ctx.accounts.global_state.accrued_interest_numerator;
 
     // This formula works with linear case, if calc_fee formula updated with duration, this part also needs to updated
-    let min_duration = ctx.accounts.sub_offer.loan_duration / 2;
+    let min_duration = ctx.accounts.sub_offer.loan_duration
+        .checked_mul(min_repaid_numerator)
+        .unwrap()
+        .checked_div(denominator)
+        .unwrap();
     let mut duration = current_time.checked_sub(started_time).unwrap();
     if duration < min_duration {
         duration = min_duration;
