0b8defe0b38470a28aad5452f9169da3b2fd9c0e
ixmorrow <ivanmorrow14@gmail.com>
2022-08-15 20:47:25 -0500

    separated state

---
+++ programs/unloc-staking/src/states/mod.rs
@@ -0,0 +1,11 @@
+pub mod user_account;
+pub use user_account::*;
+
+pub mod farmpool_account;
+pub use farmpool_account::*;
+
+pub mod extra_rewards_account;
+pub use extra_rewards_account::*;
+
+pub mod state_account;
+pub use state_account::*;
