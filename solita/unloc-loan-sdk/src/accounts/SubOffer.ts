ad141eef561532aca371963e2021ab5139316125
pprasko <prasinar@gmail.com>
2022-09-01 12:46:00 +0200

    update: bump solita, add gpa builders

--- solita/unloc-loan-sdk/src/accounts/SubOffer.ts
+++ solita/unloc-loan-sdk/src/accounts/SubOffer.ts
@@ -153,6 +153,20 @@ export class SubOffer implements SubOfferArgs {
   }
 
   /**
+   * Provides a {@link web3.Connection.getProgramAccounts} config builder,
+   * to fetch accounts matching filters that can be specified via that builder.
+   *
+   * @param programId - the program that owns the accounts we are filtering
+   */
+  static gpaBuilder(
+    programId: web3.PublicKey = new web3.PublicKey(
+      '6oVXrGCdtnTUR6xCvn2Z3f2CYaiboAGar1DKxzeX8QYh'
+    )
+  ) {
+    return beetSolana.GpaBuilder.fromStruct(programId, subOfferBeet)
+  }
+
+  /**
    * Deserializes the {@link SubOffer} from the provided data Buffer.
    * @returns a tuple of the account data and the offset up to which the buffer was read to obtain it.
    */
--- solita/unloc-loan-sdk/src/accounts/index.ts
+++ solita/unloc-loan-sdk/src/accounts/index.ts
@@ -1,3 +1,9 @@
 export * from './GlobalState'
 export * from './Offer'
 export * from './SubOffer'
+
+import { GlobalState } from './GlobalState'
+import { Offer } from './Offer'
+import { SubOffer } from './SubOffer'
+
+export const accountProviders = { GlobalState, Offer, SubOffer }
