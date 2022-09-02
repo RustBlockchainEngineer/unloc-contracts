81893b45c10a0d6a62d0d2273250ae56685dff2b
ixmorrow <ivanmorrow14@gmail.com>
2022-09-01 21:37:41 -0500

    added utc helper functions

--- programs/unloc-loan/src/constant.rs
+++ programs/unloc-loan/src/constant.rs
@@ -61,3 +61,6 @@ pub const CHAINLINK_USDC_FEED: &str = "7CLo1BY41BHAVnEs57kzYMnWXyBJrVEBPpZyQyPo2
 pub const INITIAL_OWNER: &str = "HV2t9B2oxdtkwbZrWj1vjZ2q3g4SH5rasGw8WohBFbvH";
 
 pub const UNIX_DAY: i64 = 86400;
+
+// Hour liquidity mining redemptions will be reset in UTC
+pub const REDEMPTION_RESET: i64 = 5;
--- programs/unloc-loan/src/utils.rs
+++ programs/unloc-loan/src/utils.rs
@@ -1,4 +1,4 @@
-use crate::{error::*, states::*};
+use crate::{error::*, states::*, constant::{REDEMPTION_RESET}};
 use anchor_lang::prelude::*;
 use chainlink_solana as chainlink;
 use std::convert::TryInto;
@@ -194,4 +194,149 @@ impl SafeCalc<u128> for u128 {
         }
         Ok(result.unwrap())
     }
+}
+
+pub fn convert_unix_to_utc(mut unix_time: i64) -> Result<UtcTime> {
+    /* calculate minutes */
+    let mut minutes = unix_time / 60;
+    unix_time -= minutes * 60;
+    /* calculate hours */
+    let mut hours = minutes / 60;
+    minutes -= hours * 60;
+    /* calculate days */
+    let mut days = hours / 24;
+    hours -= days * 24;
+
+     /* Unix time starts in 1970 on a Thursday */
+    let mut year: i64 = 1970;
+    let mut day_of_week: i64 = 4;
+    let mut month: i64 = 0;
+    let mut days_in_current_month: i64 = 0;
+
+    loop {
+        let mut leap_year: bool = false;
+        let days_in_year: i64;
+        if year % 4 == 0 && (year % 100 != 0 || year % 400 == 0) { leap_year = true };
+        if leap_year { days_in_year = 366 } else { days_in_year = 365 };
+        if days >= days_in_year {
+            if leap_year { day_of_week += 2 } else { day_of_week += 1 };
+            days -= days_in_year;
+            if day_of_week >= 7 {
+                day_of_week -= 7;
+            }
+            year += 1;
+        }
+        else {
+            //tm->tm_yday = days;
+            day_of_week += days;
+            day_of_week %= 7;
+
+            /* calculate the month and day */
+            let days_in_month = vec![31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+            for i in 0..12 {
+                let mut dim = days_in_month[i];
+                /* add a day to feburary if this is a leap year */
+                if i == 1 && leap_year {
+                    dim += 1;
+                }
+
+                if days >= dim {
+                    days -= dim;
+                }
+                else {
+                    month = i as i64;
+                    days_in_current_month = dim as i64;
+                    month += 1;
+                    break;
+                }
+            }
+            break;
+        }
+    }
+
+    let current_time = UtcTime {
+        days_in_month: days_in_current_month,
+        sec: unix_time,
+        min: minutes,
+        hr: hours,
+        day: days + 1,
+        month: month,
+        year: year
+    };
+
+    Ok(current_time)
+}
+
+pub fn validate_redeem_time(last_redemption: i64, current_unix: i64) -> Result<bool> {
+    let is_valid_redemption: bool;
+    let last_redemption_utc = convert_unix_to_utc(last_redemption).unwrap();
+    let current_utc = convert_unix_to_utc(current_unix).unwrap();
+
+    msg!("Last redemption utc: {:?}", last_redemption_utc);
+    msg!("Current time utc: {:?}", current_utc);
+    if last_redemption_utc.year < current_utc.year {
+        if current_utc.month == 1 && last_redemption_utc.month == 12 {
+            if current_utc.day == 1 && last_redemption_utc.day == last_redemption_utc.days_in_month {
+                if last_redemption_utc.hr < REDEMPTION_RESET || current_utc.hr >= REDEMPTION_RESET {
+                    is_valid_redemption = true;
+                } else {
+                    is_valid_redemption = false;
+                }
+            } else {
+                is_valid_redemption = true;
+            }
+        } else {
+            is_valid_redemption = true;
+        }
+    }
+    else if last_redemption_utc.year == current_utc.year && last_redemption_utc.month < current_utc.month {
+        if current_utc.month - last_redemption_utc.month == 1 {
+            if current_utc.day == 1 && last_redemption_utc.day == last_redemption_utc.days_in_month {
+                if last_redemption_utc.hr < REDEMPTION_RESET || current_utc.hr >= REDEMPTION_RESET {
+                    is_valid_redemption = true;
+                } else {
+                    is_valid_redemption = false;
+                }
+            } else {
+                is_valid_redemption = true;
+            }
+        } else {
+            is_valid_redemption = true;
+        }
+    }
+    else if last_redemption_utc.month == current_utc.month && last_redemption_utc.day < current_utc.day {
+        if current_utc.day - last_redemption_utc.day == 1 {
+            if last_redemption_utc.hr < REDEMPTION_RESET || current_utc.hr >= REDEMPTION_RESET {
+                is_valid_redemption = true;
+            } else {
+                is_valid_redemption = false;
+            }
+        } else {
+            is_valid_redemption = true;
+        }
+    }
+    else if last_redemption_utc.day == current_utc.day && last_redemption_utc.hr < REDEMPTION_RESET && current_utc.hr >= REDEMPTION_RESET {
+        is_valid_redemption = true;
+    }
+    // // TEST !!!
+    // else if last_redemption_utc.hr == current_utc.hr && last_redemption_utc.min < current_utc.min {
+    //     is_valid_redemption = true;
+    // }
+    else {
+        is_valid_redemption = false;
+    }
+
+    Ok(is_valid_redemption)
+    
+}
+
+#[derive(Debug)]
+pub struct UtcTime {
+    days_in_month: i64,
+    sec: i64,
+    min: i64,
+    hr: i64,
+    day: i64,
+    month: i64,
+    year: i64
 }
