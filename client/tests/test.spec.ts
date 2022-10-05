6b6ebe6dba97adbc11bbd4f1827e204dea9336da
SolanaEngineer <topstack2021a@gmail.com>
2022-10-05 15:08:15 +0300

    fix: path problem of scripts

--- client/tests/test.spec.ts
+++ client/tests/test.spec.ts
@@ -1,11 +1,11 @@
-import a from './1-global-init.specpartial'
-import b from './2-staking-common.specpartial'
-import c from './3-voting-common.specpartial'
-import d from './4-loan-common.specpartial'
-import e from './5-buyback-common.specpartial'
+import script1 from './1-global-init.specpartial'
+import script2 from './2-staking-common.specpartial'
+import script3 from './3-voting-common.specpartial'
+import script4 from './4-loan-common.specpartial'
+import script5 from './5-buyback-common.specpartial'
 
-a()
-b()
-c()
-d()
-e()
+script1()
+script2()
+script3()
+script4()
+script5()
